2ba10678213545453db86e9ed4a47f7105648d34
hayyantahirr <hayyantahir7@gmail.com>
2024-09-07 19:43:29 +0500

    map installed location configured

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,3 +1,12 @@
+import { View } from "react-native";
+import style from "../../styles/home-css";
+import MapView from "react-native-maps";
 export default function HomeScreen() {
-  return <></>;
+  return (
+    <>
+      <View style={style.mapContainer}>
+        <MapView style={style.map} />
+      </View>
+    </>
+  );
 }
--- styles/home-css.jsx
+++ styles/home-css.jsx
@@ -1,22 +1,12 @@
-
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
+const style = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
-export default styles;
+export default style;
