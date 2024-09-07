712be4891980e9fff5e4843c5cfc6deac3369e76
hayyantahirr <hayyantahir7@gmail.com>
2024-09-07 18:57:11 +0500

    tab from the bottom removed

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,29 +1,23 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
+import { Stack } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
+    <Stack
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
-      <Tabs.Screen
+      }}
+    >
+      <Stack.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
         }}
       />
-     
-    </Tabs>
+      {/* Add other screens here if needed */}
+    </Stack>
   );
 }
