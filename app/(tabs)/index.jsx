aefd647a13820154055ed1581884bdff9726b68b
hayyantahirr <hayyantahir7@gmail.com>
2024-09-09 21:08:03 +0500

    rides option added

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,217 +1,232 @@
-import { View, TextInput, Text, TouchableOpacity } from "react-native";
+import { View, TextInput, Text, TouchableOpacity, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import style from "../../styles/home-css";
 
 export default function HomeScreen() {
-  // State to store the user's current location
   const [location, setLocation] = useState(null);
-
-  // State to store any error message related to location permissions
   const [errorMsg, setErrorMsg] = useState(null);
-
-  // State to store search results for the pickup location
   const [searchResult, setSearchResult] = useState(null);
-
-  // State to store search results for the drop-off location
   const [dropResult, setDropResult] = useState(null);
-
-  // State to store the selected pickup location
   const [pickupLocation, setPickupLocation] = useState(null);
-
-  // State to store the selected drop-off location
   const [dropOffLocation, setDropOffLocation] = useState(null);
-
-  // State to store the value of the pickup input field
   const [pickupInput, setPickupInput] = useState("");
-
-  // State to store the value of the drop-off input field
   const [dropOffInput, setDropOffInput] = useState("");
 
-  // useEffect to request location permissions and watch the user's location
   useEffect(() => {
     (async () => {
-      // Request location permission from the user
       let { status } = await Location.requestForegroundPermissionsAsync();
-
-      // If permission is denied, set an error message and exit the function
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
         return;
       }
-
-      // Set options for location tracking
       const options = {
-        accuracy: 5, // High accuracy for location data
-        distanceInterval: 0.7, // Update location every 0.7 meters
+        accuracy: 4,
+        distanceInterval: 1,
       };
-
-      // Watch the user's location and update the state with the new location data
       Location.watchPositionAsync(options, (location) => {
-        setLocation(location); // Update the location state with the new location
+        setLocation(location);
       });
     })();
-  }, []); // Empty dependency array ensures this runs only once when the component mounts
+  }, []);
 
-  // Function to find the pickup location based on user input
   function findingPickupLocation(picklocation) {
-    setPickupInput(picklocation); // Update the input field value
+    setPickupInput(picklocation);
     const options = {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization: "fsq3gGfP49aMQlZ7kjUY/iVAkWpRdOiW6FJ3IoyTXRS2SP4=", // API key for Foursquare Places API
+        Authorization: "fsq3gGfP49aMQlZ7kjUY/iVAkWpRdOiW6FJ3IoyTXRS2SP4=",
       },
     };
 
-    // Fetch search results from Foursquare API based on the user's input and current location
     fetch(
       `https://api.foursquare.com/v3/places/search?query=${picklocation}&ll=${location.coords.latitude},${location.coords.longitude}&radius=8000`,
       options
     )
-      .then((response) => response.json()) // Convert the response to JSON
-      .then((response) => setSearchResult(response.results)) // Update search results state with the fetched data
-      .catch((err) => console.error(err)); // Handle any errors that occur during the fetch
+      .then((response) => response.json())
+      .then((response) => setSearchResult(response.results))
+      .catch((err) => console.error(err));
   }
 
-  // Function to find the drop-off location based on user input
   function findingDropOffLocation(dropLocation) {
-    setDropOffInput(dropLocation); // Update the input field value
+    setDropOffInput(dropLocation);
     const options = {
       method: "GET",
       headers: {
         accept: "application/json",
-        Authorization: "fsq3gGfP49aMQlZ7kjUY/iVAkWpRdOiW6FJ3IoyTXRS2SP4=", // API key for Foursquare Places API
+        Authorization: "fsq3gGfP49aMQlZ7kjUY/iVAkWpRdOiW6FJ3IoyTXRS2SP4=",
       },
     };
 
-    // Fetch search results from Foursquare API based on the user's input and current location
     fetch(
       `https://api.foursquare.com/v3/places/search?query=${dropLocation}&ll=${location.coords.latitude},${location.coords.longitude}&radius=8000`,
       options
     )
-      .then((response) => response.json()) // Convert the response to JSON
-      .then((response) => setDropResult(response.results)) // Update drop-off search results state with the fetched data
-      .catch((err) => console.error(err)); // Handle any errors that occur during the fetch
+      .then((response) => response.json())
+      .then((response) => setDropResult(response.results))
+      .catch((err) => console.error(err));
   }
 
-  // Function to remove the selected pickup location
   function removePickup() {
-    setPickupLocation(null); // Clear the selected pickup location
-    setPickupInput(""); // Clear the input field value
+    setPickupLocation(null);
+    setPickupInput("");
   }
 
-  // Function to remove the selected drop-off location
   function removeDropOff() {
-    setDropOffLocation(null); // Clear the selected drop-off location
-    setDropOffInput(""); // Clear the input field value
+    setDropOffLocation(null);
+    setDropOffInput("");
   }
 
   return (
-    <View style={style.mapContainer}>
-      {/* Display the map only if the location is available */}
+    <View style={style.container}>
       {location && (
         <MapView
           style={style.map}
           region={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0032,
-            longitudeDelta: 0.0001,
+            latitude: pickupLocation
+              ? pickupLocation.geocodes.main.latitude
+              : location.coords.latitude,
+            longitude: pickupLocation
+              ? pickupLocation.geocodes.main.longitude
+              : location.coords.longitude,
+            latitudeDelta: pickupLocation ? 0.10 : 0.0032,
+            longitudeDelta: pickupLocation ? 0.10 : 0.0001,
           }}
         >
-          {/* Marker to show the user's current location on the map */}
-          <Marker coordinate={location.coords} />
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
 
-          {/* Container for the input fields and search results */}
-          <View style={style.inputContainer}>
-            {/* Input field for searching pickup locations */}
-            <TextInput
-              placeholder="Search Pickup location"
-              onChangeText={findingPickupLocation}
-              value={pickupInput}
-              style={style.input}
-              editable={!pickupLocation} // Disable input if pickup location is selected
+          {/* Add a marker for the drop-off location if it is selected */}
+          {dropOffLocation && (
+            <Marker
+              coordinate={{
+                latitude: dropOffLocation.geocodes.main.latitude,
+                longitude: dropOffLocation.geocodes.main.longitude,
+              }}
+              title="Drop-Off Location"
+              description={dropOffLocation.location.formatted_address}
             />
-            {/* Display search results if available and no pickup location is selected */}
-            {searchResult && !pickupLocation && (
-              <View style={style.searchResultContainer}>
-                {searchResult.map((item, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                      setPickupLocation(item);
-                      setPickupInput(""); // Clear the input field after selecting a location
-                    }}
-                  >
-                    <Text style={style.searchResultText}>
-                      {item.name} | {item.location.formatted_address}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            )}
-            {/* Display selected pickup location with a remove button */}
-            {pickupLocation && (
-              <View style={style.selectedLocationContainer}>
-                <Text style={style.selectedLocationText}>
-                  Pickup Location: {pickupLocation.name}
-                </Text>
-                <TouchableOpacity
-                  onPress={removePickup}
-                  style={style.removeButton}
-                >
-                  <Text style={style.removeButtonText}>Remove</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-
-            {/* Input field for searching drop-off locations */}
-            <TextInput
-              placeholder="Search DropOff location"
-              onChangeText={findingDropOffLocation}
-              value={dropOffInput}
-              style={style.input}
-              editable={!dropOffLocation} // Disable input if drop-off location is selected
-            />
-            {/* Display search results if available and no drop-off location is selected */}
-            {dropResult && !dropOffLocation && (
-              <View style={style.searchResultContainer}>
-                {dropResult.map((item, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                      setDropOffLocation(item);
-                      setDropOffInput(""); // Clear the input field after selecting a location
-                    }}
-                  >
-                    <Text style={style.searchResultText}>
-                      {item.name} | {item.location.formatted_address}
-                    </Text>
-                  </TouchableOpacity>
-                ))}
-              </View>
-            )}
-            {/* Display selected drop-off location with a remove button */}
-            {dropOffLocation && (
-              <View style={style.selectedLocationContainer}>
-                <Text style={style.selectedLocationText}>
-                  DropOff Location: {dropOffLocation.name}
-                </Text>
-                <TouchableOpacity
-                  onPress={removeDropOff}
-                  style={style.removeButton}
-                >
-                  <Text style={style.removeButtonText}>Remove</Text>
-                </TouchableOpacity>
-              </View>
-            )}
-          </View>
+          )}
         </MapView>
       )}
 
-      {/* Display an error message if there is an issue with location permissions */}
+      <View style={style.inputContainer}>
+        <View style={style.vehicleButtonContainer}>
+          <TouchableOpacity style={style.vehicleButton}>
+            <Image
+              style={style.vehicleImage}
+              source={require("../../assets/GoFleet Images/GoFleet Premium.png")}
+            />
+            <Text style={style.vehicleButtonText}>GoFleet Premium</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.vehicleButton}>
+            <Image
+              style={style.vehicleImage}
+              source={require("../../assets/GoFleet Images/Fleet Mini.png")}
+            />
+            <Text style={style.vehicleButtonText}>Fleet Mini</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.vehicleButton}>
+            <Image
+              style={style.vehicleImage}
+              source={require("../../assets/GoFleet Images/Rickshaw.png")}
+            />
+            <Text style={style.vehicleButtonText}>Rikshaw</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.vehicleButton}>
+            <Image
+              style={style.vehicleImage}
+              source={require("../../assets/GoFleet Images/Bike.png")}
+            />
+            <Text style={style.vehicleButtonText}>Bike</Text>
+          </TouchableOpacity>
+        </View>
+
+        {!pickupLocation && (
+          <TextInput
+            placeholder="Search Pickup location"
+            onChangeText={findingPickupLocation}
+            value={pickupInput}
+            style={style.input}
+          />
+        )}
+        {searchResult && !pickupLocation && (
+          <View style={style.searchResultContainer}>
+            {searchResult.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  setPickupLocation(item);
+                  setPickupInput("");
+                }}
+              >
+                <Text style={style.searchResultText}>
+                  {item.name} | {item.location.formatted_address}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+        {pickupLocation && (
+          <View style={style.selectedLocationContainer}>
+            <Text style={style.selectedLocationText}>
+              Pickup Location: {pickupLocation.name}
+            </Text>
+            <TouchableOpacity onPress={removePickup} style={style.removeButton}>
+              <Text style={style.removeButtonText}>Remove</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        {!dropOffLocation && (
+          <TextInput
+            placeholder="Search DropOff location"
+            onChangeText={findingDropOffLocation}
+            value={dropOffInput}
+            style={style.input}
+          />
+        )}
+        {dropResult && !dropOffLocation && (
+          <View style={style.searchResultContainer}>
+            {dropResult.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  setDropOffLocation(item);
+                  setDropOffInput("");
+                }}
+              >
+                <Text style={style.searchResultText}>
+                  {item.name} | {item.location.formatted_address}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+        {dropOffLocation && (
+          <View style={style.selectedLocationContainer}>
+            <Text style={style.selectedLocationText}>
+              DropOff Location: {dropOffLocation.name}
+            </Text>
+            <TouchableOpacity
+              onPress={removeDropOff}
+              style={style.removeButton}
+            >
+              <Text style={style.removeButtonText}>Remove</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        <TouchableOpacity style={style.findingRideButton}>
+          <Text style={style.findingRideText}>Find Ride</Text>
+        </TouchableOpacity>
+      </View>
       {errorMsg && <Text>{errorMsg}</Text>}
     </View>
   );
--- styles/home-css.jsx
+++ styles/home-css.jsx
@@ -1,72 +1,103 @@
 import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create({
-  mapContainer: {
+  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // borderColor: "red",
+    // borderWidth: 2,
+    boxsizing: "border-box",
   },
   map: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
   },
   inputContainer: {
+    position: "absolute",
+    bottom: 0,
     width: "100%",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 20,
+  },
+  vehicleImage: {
+    width: 50,
+    height: 35,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 8,
+  },
+  vehicleButtonContainer: {
+    flexDirection: "row",
+    gap: 5,
+    marginLeft: 0,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  vehicleButton: {
+    width: "25%",
+    height: 80,
+    // borderColor: "black",
+    // borderWidth: 1,
+  },
+  vehicleButtonText: {
+    textAlign: "center",
+    fontSize: 14,
+    fontWeight: "bold",
   },
   input: {
-    marginTop: 40,
-
     height: 40,
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
     backgroundColor: "white",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "80%",
+    marginBottom: 10,
   },
   searchResultContainer: {
-    marginTop: 10,
     backgroundColor: "white",
     borderRadius: 10,
-    // padding: 10,
+    marginBottom: 10,
   },
   searchResultText: {
-    // paddingVertical: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
     borderColor: "black",
     borderWidth: 1,
     fontSize: 16,
   },
-  address : {
-    fontSize: 10,
-    opacity: 0.5
-  },
   selectedLocationContainer: {
-    marginTop: 5,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
     backgroundColor: "white",
     height: 40,
-    // textAlign: "center",
-    width: "80%",
-    marginLeft: "auto",
-    marginRight: "auto",
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 10,
   },
   selectedLocationText: {
     fontSize: 16,
-    // textAlign: "center",
-    marginLeft: "auto",
   },
   removeButton: {
-   marginRight:"auto",
-   marginLeft:"auto",
-    
-    
-    
+    marginLeft: 10,
   },
   removeButtonText: {
     color: "red",
+    fontWeight: "bold",
+  },
+  findingRideButton: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: "100%",
+    backgroundColor: "red",
+
+    // borderColor: "black",
+    // borderWidth: 1,
+    borderRadius: 15,
+  },
+  findingRideText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
     fontWeight: "bold",
   },
 });
